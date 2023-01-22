00cffc1ae6f827107cca74d3c6f4cab76efbf35d
tybo-dev <tobias@diez.guru>
2023-01-22 20:31:17 +0100

    theme as context provider added

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component { 
+    render() { 
+        return (
+            <div className="book-list">
+                <ul>
+                    <li>Sapiens</li>
+                    <li>Homo Deus</li>
+                    <li>Dogs4Life</li>
+                </ul>
+            </div>
+        );
+    }
+}
+ 
+export default Booklist;
