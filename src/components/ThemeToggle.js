f94a783343b124c7c74e669e16a8d2fa3c7ffc61
tybo-dev <tobias@diez.guru>
2023-01-24 21:15:09 +0100

    testing

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,35 +1,24 @@
-import React, { Component } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
-
-
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 import { useThemeContext } from "../contexts/ThemeContext";
 
 function ThemeToggle() {
-  const { theme } = useThemeContext();
+  const { theme, toggleTheme} = useThemeContext();
   console.log(theme.isLightTheme);
 
-  const toggleTheme = () => {
-    theme.isLightTheme = false;
-    console.log(theme.isLightTheme);
-  }
-
-  return (
-                <button onClick={toggleTheme}>ToggleTheme</button>
-            );
+ 
+  return <button onClick={toggleTheme}>ToggleTheme</button>;
 }
-
-
-
 
 // class ThemeToggle extends Component {
 //     static  contextType = ThemeContext;
-//     render() { 
+//     render() {
 //         const { toggleTheme } = this.context;
 //         return (
 //             <button onClick={toggleTheme}>ToggleTheme</button>
 //         );
 //     }
 // }
- 
+
 export default ThemeToggle;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,40 +1,44 @@
-import React, { createContext, useContext, Component, useState } from 'react';
+import React, { createContext, useContext, Component, useState } from "react";
 
 export const ThemeContext = createContext();
 
 export const useThemeContext = () => useContext(ThemeContext);
 
-export default function ThemeProvier({children}) {
-    const [theme, setTheme] = useState({ 
-        isLightTheme: true,
-        light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
-        dark: { syntax: "#ddd", ui: "#333", bg: "555" }
-    });
 
-   
 
-    return (
-        <ThemeContext.Provider value={{theme, setTheme}}>
-            {children}
-        </ThemeContext.Provider>
-    )
-    
+export default function ThemeProvier({ children }) {
+  const [theme, setTheme] = useState({
+    isLightTheme: true,
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "555" },
+  });
+
+  const toggleTheme = () => {
+    //    this.setState({ isLightTheme: !this.state.isLightTheme });
+  
+    if (theme.isLightTheme) theme.isLightTheme = false;
+    else theme.isLightTheme = true;
+    console.log(theme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
 
-
-
-
 // class ThemeContextProvider extends Component {
-//     state = { 
+//     state = {
 //         isLightTheme: true,
 //         light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
 //         dark: { syntax: "#ddd", ui: "#333", bg: "555" }
-//      } 
+//      }
 //      toggleTheme = () => {
 //         this.setState({ isLightTheme: !this.state.isLightTheme });
 //      }
 
-//     render() { 
+//     render() {
 //         return (
 //             <ThemeContext.Provider value={{...this.state, toggleTheme: this.toggleTheme}}>
 //               {this.props.children}
@@ -42,5 +46,5 @@ export default function ThemeProvier({children}) {
 //         );
 //     }
 // }
- 
+
 // export default ThemeContextProvider;
